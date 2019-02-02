b2b0bb49f9c9310d709b1c74fb9d2d818b5ff023
naresh reddy etikyala <nareshreddy.etikyala@gmail.com>
2019-02-02 18:20:07 +0100

    first

--- searchword.js
+++ searchword.js
@@ -1,3 +1,19 @@
 <script>
-var search = document.getElementByid("");
+var searchinput = document.querySelector("#id");
+searchinput.addEventListener('keyup', function(e) {
+    var searchchar = e.target.value.toUpperCase();
+    var notes = ul.getElementsByTagName("li");
+    //start searching words in already exist columns
+
+    Array.from(notes).forEach(function(note)){
+        var parText = note.firstelementChild.textContent;
+
+        if(parText.toUpperCase().indexOf(searchchar) !== -1){
+            note.style.display="block";
+        } else {
+            note.style.display = "none";
+        }
+    });
+    
+})
 </script>
